b19b9b8d82e05c86e926aefe8540a02a2528b578
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-09 08:32:30 -0300

    FEAT: add icons in footerComponent

--- src/components/globals/FooterComponent.jsx
+++ src/components/globals/FooterComponent.jsx
@@ -2,7 +2,11 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import globalStyle from '../../styles/global/globalStyle';
 import colors from '../../styles/global/colors';
-import { default as navbarItems } from '../../constants/navbarItems.json';
+import pinIcon from '../footer/pin.svg';
+import mailIcon from '../footer/mail.svg';
+import phoneIcon from '../footer/phone.svg';
+import linkedinIcon from '../footer/linkedinIcon.svg';
+import instagramIcon from '../footer/instagramIcon.svg';
 
 
 export const FooterComponent = ({ footerView }) => {
@@ -11,46 +15,66 @@ export const FooterComponent = ({ footerView }) => {
 
   return (
     <div className={globalStyles.footerComponent}>
-      <Grid container direction="row" justify="flex-end" alignItems="baseline">
-        <Grid item md={3}>
-          <p style={{ fontWeight: 'bold' }}> {footerView.address.title} </p>
-          <p>
-            <a target='_blank' href={footerView.address.link} rel="noreferrer" style={{ color: 'inherit', textDecoration: 'none' }}>
-              {footerView.address.description}
-            </a>
-          </p>
+      <Grid container direction="row" justify="space-evenly" alignItems="baseline">
+        <Grid item xs={10} md={3}>
+          <Grid container direction="row" justify="flex-start" alignItems="center" >
+            <Grid item xs={2} style={{ textAlign: 'center' }} >
+              <img src={pinIcon} alt={`Photio-footerItem-${footerView.address.title}`} />
+            </Grid>
+            <Grid item xs={10}>
+              <p style={{ fontWeight: 'bold' }}> {footerView.address.title} </p>
+              <p>
+                <a target='_blank' href={footerView.address.link} rel="noreferrer" style={{ color: 'inherit', textDecoration: 'none' }}>
+                  {footerView.address.description}
+                </a>
+              </p>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item md={3}>
-          <p style={{ fontWeight: 'bold' }}> {footerView.mail.title} </p>
-          <p>
-            <a href={`mailto:${footerView.mail.description}`} style={{ color: 'inherit', textDecoration: 'none' }}>{
-              footerView.mail.description}
-            </a>
-          </p>
+        <Grid item xs={10} md={3}>
+          <Grid container direction="row" justify="flex-start" alignItems="center" >
+            <Grid item xs={2} style={{ textAlign: 'center' }} >
+              <img src={mailIcon} alt={`Photio-footerItem-${footerView.mail.title}`} />
+            </Grid>
+            <Grid item xs={10}>
+              <p style={{ fontWeight: 'bold' }}> {footerView.mail.title} </p>
+              <p>
+                <a href={`mailto:${footerView.mail.description}`} style={{ color: 'inherit', textDecoration: 'none' }}>{
+                  footerView.mail.description}
+                </a>
+              </p>
+            </Grid>
+          </Grid>
+
         </Grid>
-        <Grid item md={2}>
-          <p style={{ fontWeight: 'bold' }}> {footerView.phone.title} </p>
-          <p>
-            <a href={`tel:${footerView.phone.description}`} style={{color: 'inherit', textDecoration: 'none'}}>
-              {footerView.phone.description}
-            </a>
-          </p>
-        </Grid>
-        <Grid item md={2} style={{ textAlign: 'right', marginRight: '50px' }}>
-          {
-            navbarItems.map((menuItem, index) => {
-              return (
-                <p key={`menuItem-${index}`}>{menuItem.title}</p>
-              )
-            })
-          }
-          <p style={{ fontWeight: 'bold', letterSpacing: '6px' }}>{footerView.rrss.title}</p>
+        <Grid item xs={10} md={2}>
+          <Grid container direction="row" justify="flex-start" alignItems="center" >
+            <Grid item xs={2} style={{ textAlign: 'center' }} >
+              <img src={phoneIcon} alt={`Photio-footerItem-${footerView.phone.title}`} />
+            </Grid>
+            <Grid item xs={10}>
+              <p style={{ fontWeight: 'bold' }}> {footerView.phone.title} </p>
+              <p>
+                <a href={`tel:${footerView.phone.description}`} style={{ color: 'inherit', textDecoration: 'none' }}>
+                  {footerView.phone.description}
+                </a>
+              </p>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
 
-      <Grid container direction="row" justify="center" alignItems="flex-end">
-        <Grid item xs={12}>
-          <p style={{ color: colors.gray_4, textAlign: 'center' }}><span style={{ color: colors.gray_3, fontWeight: 'Bold' }}> FRACTAL - </span>Copyright 2021. Todos los derechos reservados.</p>
+      <Grid container direction="row" justify="center" alignItems="flex-end" style={{ marginTop: '20px' }}>
+        <Grid item xs={10} style={{ textAlign: 'center' }}>
+          <p style={{ fontWeight: 'bold', letterSpacing: '6px' }}>
+            <a target='_blank' href='https://www.linkedin.com/company/photio/' rel="noreferrer" style={{ color: 'inherit', textDecoration: 'none' }}>
+              <img src={linkedinIcon} alt="Photio-footerItem-Linkedin" style={{marginRight: '10px'}} />
+            </a>
+            <a target='_blank' href='https://www.instagram.com/photio.nano' rel="noreferrer" style={{ color: 'inherit', textDecoration: 'none' }}>
+              <img src={instagramIcon} alt="Photio-footerItem-Instagram" style={{marginRight: '10px'}} />
+            </a>
+            {footerView.rrss.title}</p>
+          <p style={{ color: colors.gray_4 }}><span style={{ color: colors.gray_3, fontWeight: 'Bold' }}> FRACTAL - </span>Copyright 2021. Todos los derechos reservados.</p>
         </Grid>
       </Grid>
     </div>
