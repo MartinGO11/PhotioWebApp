438efe870a69a81065c6fd5e06ef5296b014d4c6
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-09 17:14:40 -0300

    REFACTOR: changes styles to mobile version in odsComponent

--- src/components/home/OdsComponent.jsx
+++ src/components/home/OdsComponent.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Grid, Hidden } from '@material-ui/core';
 import globalStyle from '../../styles/global/globalStyle';
 import homeStyle from '../../styles/global/homeStyle';
-import { default as odsBanner } from './odsBanner.svg';
-import { default as ods } from './ods.svg'
-import { default as odsDesktop } from './odsDesktop.svg'
+import { default as odsBannerDesktop } from './odsBannerDesktop.png';
+import { default as odsDesktop } from './odsDesktop.svg';
+import { default as odsBannerMobile } from './odsBannerMobile.png';
 
 export const OdsComponent = ({ odsText }) => {
 
@@ -15,12 +15,9 @@ export const OdsComponent = ({ odsText }) => {
     <>
       <Hidden smDown>
         <Grid container direction="row" justify="center" alignItems="center" spacing={1} className={homeStyles.odsComponent} >
-          <Grid item md={1} >
-            {/* vacio */}
-          </Grid>
           <Grid item md={3}>
             <p className={globalStyles.paragraphImportant} style={{ marginLeft: '50px' }}>{odsText.description}</p>
-            <img src={odsBanner} alt="odsBanner" style={{ marginLeft: '50px', maxWidth: '100%' }} />
+            <img src={odsBannerDesktop} alt="odsBanner" style={{ marginLeft: '50px', maxWidth: '100%' }} />
           </Grid>
           <Grid item md={2} >
             {/* vacio */}
@@ -33,11 +30,11 @@ export const OdsComponent = ({ odsText }) => {
 
       <Hidden mdUp>
         <Grid container direction="row" justify="center" alignItems="center" spacing={1} className={globalStyles.principalComponent} >
-          <Grid item xs={8} className={globalStyles.center}>
-            <p className={globalStyles.paragraphImportant}>{odsText.description}</p>
+          <Grid item xs={10} className={globalStyles.center}>
+            <p style={{fontSize: '16px', marginBottom: '0px'}}>{odsText.description}</p>
           </Grid>
           <Grid item xs={8} className={globalStyles.center}>
-            <img src={odsBanner} alt="odsBanner" />
+            <img src={odsBannerMobile} alt="odsBanner" />
           </Grid>
           <Grid item xs={8} className={globalStyles.center}>
           <img src={odsDesktop} alt="ods" style={{maxWidth: '100%' }} />
