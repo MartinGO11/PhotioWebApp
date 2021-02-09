a97ef53cc911b8987d9a06bfeb9412313ed9782d
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-09 08:33:15 -0300

    FEAT: add styles to mobile devices in SliderComponent

--- src/components/globals/SliderComponent.jsx
+++ src/components/globals/SliderComponent.jsx
@@ -1,3 +1,4 @@
+import { Hidden } from '@material-ui/core';
 import React from 'react';
 
 export const SliderComponent = ({sliderContent}) => {
@@ -10,6 +11,7 @@ export const SliderComponent = ({sliderContent}) => {
       backgroundImage: `url(${sliderContent.background})`,
       height: '90vh',
     }}>
+      <Hidden smDown>
       <p style={{
         color: 'white',
         fontSize: '18px',
@@ -19,9 +21,26 @@ export const SliderComponent = ({sliderContent}) => {
         top: '80%',
         left: '60%',
         marginRight: '10vw',
+        textAlign: 'left'
       }}>
         {sliderContent.description}
       </p>
+      </Hidden>
+      <Hidden mdUp>
+      <p style={{
+        color: 'white',
+        fontSize: '14px',
+        padding: '20px 30px 20px 30px',
+        background: 'rgba(24,70,102,0.8)',
+        position: 'absolute',
+        top: '70%',
+        left: '30%',
+        marginRight: '10vw',
+        textAlign: 'left'
+      }}>
+        {sliderContent.description}
+      </p>
+      </Hidden>
 
     </div>
   )
