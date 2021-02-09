d977bd53a108ad35514ebd8e8d147bb3a9d85cf4
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-09 17:06:32 -0300

    REFACTOR: create mobile component to IndicatorComponner

--- src/components/home/IndicatorComponents.jsx
+++ src/components/home/IndicatorComponents.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
 import globalStyle from '../../styles/global/globalStyle';
 import { default as paintRoll } from './paintRoll.svg';
 import { default as map } from './map.svg'
@@ -9,23 +9,36 @@ export const IndicatorComponents = ({ indicatorText }) => {
   const globalStyles = globalStyle();
 
   return (
-    <Grid container direction="row" justify="center" alignItems="center" spacing={1}  >
-      <Grid item xs={2} md={4} >
-        {/* Espacio libre */}
+    <>
+    <Hidden smDown>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={1} style={{paddingTop: '30px'}}  >
+        <Grid item xs={4} md={2} className={globalStyles.fontLato} >
+          <img src={paintRoll} alt="paintRoll" />
+          <p className={globalStyles.paragraphTitle} style={{ margin: '0px' }}>{indicatorText.meters.count}</p>
+          <p style={{ fontSize: '24px', margin: '0px' }}>{indicatorText.meters.description}</p>
+        </Grid>
+        <Grid item xs={4} md={2} className={globalStyles.fontLato} >
+          <img src={map} alt="map" />
+          <p className={globalStyles.paragraphTitle} style={{ margin: '0px' }}>{indicatorText.cities.count}</p>
+          <p style={{ fontSize: '24px', margin: '0px' }}>{indicatorText.cities.description}</p>
+        </Grid>
       </Grid>
-      <Grid item xs={4} md={2} className={globalStyles.fontLato} >
-        <img src={paintRoll} alt="paintRoll" />
-        <p className={globalStyles.paragraphTitle}>{indicatorText.meters.count}</p>
-        <p className={globalStyles.paragraph}>{indicatorText.meters.description}</p>
+    </Hidden>
+    <Hidden mdUp>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={1}  >
+        <Grid item xs={5} md={2} className={globalStyles.fontLato} >
+          <img src={paintRoll} alt="paintRoll" />
+          <p style={{ fontSize: '24px', margin: '0px', fontWeight: 'bold' }}>{indicatorText.meters.count}</p>
+          <p style={{ fontSize: '14px', margin: '0px' }}>{indicatorText.meters.description}</p>
+        </Grid>
+        <Grid item xs={5} md={2} className={globalStyles.fontLato} >
+          <img src={map} alt="map" />
+          <p style={{ fontSize: '24px', margin: '0px', fontWeight: 'bold' }}>{indicatorText.cities.count}</p>
+          <p style={{ fontSize: '14px', margin: '0px' }}>{indicatorText.cities.description}</p>
+        </Grid>
       </Grid>
-      <Grid item xs={4} md={2} className={globalStyles.fontLato} >
-        <img src={map} alt="map" />
-        <p className={globalStyles.paragraphTitle}>{indicatorText.cities.count}</p>
-        <p className={globalStyles.paragraph}>{indicatorText.cities.description}</p>
-      </Grid>
-      <Grid item xs={2} md={4} >
-        {/* Espacio libre */}
-      </Grid>
-    </Grid>
+    </Hidden>
+    </>
+
   )
 }
