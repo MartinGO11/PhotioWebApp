332166fd6919347ca6eddab8dfbf06ecb81ce1cd
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-09 16:49:11 -0300

    REFACTOR: change titleComponent sn showAction

--- src/components/globals/TitleComponent.jsx
+++ src/components/globals/TitleComponent.jsx
@@ -10,47 +10,67 @@ export const TitleComponent = ({ titleContent, titleImage, showAction = false })
   const globalStyles = globalStyle();
 
   return (
-    <Grid container direction="row" justify="flex-start" alignItems="center" spacing={1} >
-      <Grid item xs={4} md={3} style={{ textAlign: 'center' }} >
-        <img src={titleImage} alt="titleImage" style={{maxWidth: '100%'}} />
-      </Grid>
-      <Grid item xs={1} style={{ textAlign: 'right', paddingTop: '25px', paddingBottom: '25px' }}>
-        <img src={openKey} alt="openKey" style={{ marginRight: '20px', maxHeight: '100%' }} />
-      </Grid>
-      <Grid item xs={6} md={ showAction ? 4 : 6 } className={globalStyles.fontLato} >
-        {
-          titleContent.description.map((description, index) => {
-            return (
-              <p key={`title-${index}`} style={{ marginRight: '20px' }}>{description}</p>
-            )
-          })
-        }
-      </Grid>
-      {
-        showAction &&
-        <>
-          <Hidden smDown>
-            <Grid item md={4} className={globalStyles.fontPoppins} style={{ textAlign: 'center' }}>
-              <p className={globalStyles.paragraphTitle} style={{ color: colors.blue_1, fontWeight: 'bold' }}>{titleContent.titleAction}</p>
-              <p className={globalStyles.paragraph} style={{ marginLeft: '50px', marginRight: '50px' }}>{titleContent.descriptionAction}</p>
-              <ColorButton variant="contained">{titleContent.textButtonAction}</ColorButton>
+    <>
+      <Hidden smDown>
+        <Grid container direction="row" justify="flex-start" alignItems="center" spacing={1} >
+          <Grid item xs={4} md={3} style={{ textAlign: 'center' }} >
+            <img src={titleImage} alt="titleImage" style={{ maxWidth: '100%' }} />
+          </Grid>
+          <Grid item xs={1} style={{ textAlign: 'right', paddingTop: '25px', paddingBottom: '25px' }}>
+            <img src={openKey} alt="openKey" style={{ marginRight: '20px', maxHeight: '100%' }} />
+          </Grid>
+          <Grid item xs={7} md={5} className={globalStyles.fontLato} >
+            {
+              titleContent.description.map((description, index) => {
+                return (
+                  <div key={`title-${index}`} style={{ marginRight: '20px', fontSize: '18px' }}>
+                    {description}
+                  </div>
+                )
+              })
+            }
+          </Grid>
+          <Grid item md={3} className={globalStyles.fontPoppins} style={{ textAlign: 'center' }}>
+            <p className={globalStyles.paragraphTitle} style={{ color: colors.blue_1, fontWeight: 'bold', marginBottom: '0px' }}>{titleContent.titleAction}</p>
+            <p className={globalStyles.paragraph} style={{ marginLeft: '50px', marginRight: '50px', marginTop: '0px', marginBottom: '9px' }}>{titleContent.descriptionAction}</p>
+            <ColorButton variant="contained" style={{ marginBottom: '30px' }}>{titleContent.textButtonAction}</ColorButton>
+          </Grid>
+        </Grid>
+      </Hidden>
+      <Hidden mdUp>
+        <Grid container direction="row" justify="center" alignItems="center" spacing={1} >
+          <Grid item xs={3} style={{ textAlign: 'center' }} >
+            <img src={titleImage} alt="titleImage" style={{ maxWidth: '100%' }} />
+          </Grid>
+          <Grid item xs={1} style={{ textAlign: 'right', paddingTop: '25px', paddingBottom: '25px' }}>
+            <img src={openKey} alt="openKey" style={{ marginRight: '20px', maxHeight: '100%' }} />
+          </Grid>
+          <Grid item xs={7} className={globalStyles.fontLato} >
+            {
+              titleContent.description.map((description, index) => {
+                return (
+                  <div key={`title-${index}`} style={{ marginRight: '20px', fontSize: '12px' }}>
+                    {description}
+                  </div>
+                )
+              })
+            }
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justify="center" alignItems="center" spacing={1} className={`${globalStyles.fontPoppins} ${globalStyles.secondaryComponent}`} >
+            <Grid item xs={7} >
+              <p style={{ color: colors.blue_1, fontWeight: 'bold', fontSize: '18px', marginBottom: '0px' }}>{titleContent.titleAction}</p>
+              <p style={{ fontSize: '14px', marginTop: '0px' }}>{titleContent.descriptionAction}</p>
             </Grid>
-          </Hidden>
-          <Hidden mdUp>
-            <Grid container direction="row" justify="center" alignItems="center" spacing={1} className={`${globalStyles.fontPoppins} ${globalStyles.secondaryComponent}`} >
-              <Grid item xs={7} >
-                <p className={globalStyles.paragraphTitle} style={{ color: colors.blue_1, fontWeight: 'bold', marginLeft: '50px' }}>{titleContent.titleAction}</p>
-                <p className={globalStyles.paragraph} style={{ marginLeft: '50px', marginRight: '50px' }}>{titleContent.descriptionAction}</p>
-              </Grid>
-              <Grid item xs={1}>
-              </Grid>
-              <Grid item xs={4}>
-                <ColorButton variant="contained">{titleContent.textButtonAction}</ColorButton>
-              </Grid>
+            <Grid item xs={4}>
+              <ColorButton variant="contained">
+                <p style={{fontSize: '14px', margin: '0px'}}>
+                  {titleContent.textButtonAction}
+                </p>
+              </ColorButton>
             </Grid>
-          </Hidden>
-        </>
-      }
-    </Grid>
+          </Grid>
+      </Hidden>
+    </>
   )
 }
