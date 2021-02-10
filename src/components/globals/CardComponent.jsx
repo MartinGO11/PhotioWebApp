9bfb08f9ccf9d01ec66b82e2c15c06304f226e39
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-10 00:37:48 -0300

    REFACTOR: finished aboutView

--- src/components/globals/CardComponent.jsx
+++ src/components/globals/CardComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid, Hidden } from '@material-ui/core';
 import globalStyle from '../../styles/global/globalStyle';
 
-export const CardComponent = ({ cardContent, color, benefits=false }) => {
+export const CardComponent = ({ cardContent, color, benefits = false }) => {
 
   const globalStyles = globalStyle();
 
@@ -23,14 +23,17 @@ export const CardComponent = ({ cardContent, color, benefits=false }) => {
             display: 'flex',
             alignItems: 'center'
           }}>
-            <img src={cardContent.icon} alt={`Photio-Application-Icon-${cardContent.title}`} style={{marginRight: '30px'}} />
+            {
+              benefits &&
+              <img src={cardContent.icon} alt={`Photio-Application-Icon-${cardContent.title}`} style={{ marginRight: '30px' }} />
+            }
             {cardContent.title}
           </h1>
-          <div style={ benefits ? { borderLeft: `solid 8px ${color}` } : {}}>
+          <div style={benefits ? { borderLeft: `solid 8px ${color}` } : {}}>
             {
               cardContent.description.map((description, index) => {
                 return (
-                  <p key={`descriptionCard-${index}`} style={ benefits ? { paddingLeft: '10px', fontSize: '18px', maxWidth: '75%' } : {}}>{description}</p>
+                  <p key={`descriptionCard-${index}`} style={benefits ? { paddingLeft: '10px', fontSize: '18px', maxWidth: '75%' } : { fontSize: '18px', maxWidth: '75%' }}>{description}</p>
                 )
               })
             }
@@ -51,20 +54,23 @@ export const CardComponent = ({ cardContent, color, benefits=false }) => {
 
       <Hidden mdUp>
         <Grid item sm={12}>
-          <img src={cardContent.img} alt="imgCard" style={{maxWidth: '100%'}} />
+          <img src={cardContent.img} alt="imgCard" style={{ maxWidth: '100%' }} />
         </Grid>
-        <Grid item md={12} style={{background: color, color: 'white', padding: '5px 20px 5px 20px', fontFamily: 'Poppins, sans-serif', width: '100%'}}>
-          <h1 style={{fontSize: '24px'}}>
-          <img src={cardContent.icon} alt={`Photio-Application-Icon-${cardContent.title}`} style={{marginRight: '20px', maxHeight: '30px' }} />
+        <Grid item md={12} style={{ background: color, color: 'white', padding: '5px 20px 5px 20px', fontFamily: 'Poppins, sans-serif', width: '100%' }}>
+          <h1 style={{ fontSize: '24px' }}>
+            {
+              benefits &&
+              <img src={cardContent.icon} alt={`Photio-Application-Icon-${cardContent.title}`} style={{ marginRight: '20px', maxHeight: '30px' }} />
+            }
             {cardContent.title}
           </h1>
-          </Grid>
-          <Grid item xs={10} className={globalStyles.cardComponentTitleMobile}>
-          <div style={{ borderLeft: `solid 8px ${color}` }}>
+        </Grid>
+        <Grid item xs={10} className={globalStyles.cardComponentTitleMobile}>
+          <div style={benefits ? { borderLeft: `solid 8px ${color}` } : {}}>
             {
               cardContent.description.map((description, index) => {
                 return (
-                  <p key={`descriptionCard-${index}`} style={{ paddingLeft: '10px', fontFamily: 'Lato, sans-serif' }}>{description}</p>
+                  <p key={`descriptionCard-${index}`} style={ benefits ?  { paddingLeft: '10px', fontFamily: 'Lato, sans-serif' }: {fontFamily: 'Lato, sans-serif'}}>{description}</p>
                 )
               })
             }
