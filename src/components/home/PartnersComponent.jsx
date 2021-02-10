fc35d35844334228df0c0ce5e17b2b19b07efee4
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-10 11:03:36 -0300

    FEAT: add slider in partnerComponent

--- src/components/home/PartnersComponent.jsx
+++ src/components/home/PartnersComponent.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid, Hidden } from '@material-ui/core';
 import colors from '../../styles/global/colors';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import { SliderPartnerComponent } from './SliderPartnerComponent';
+
+SwiperCore.use([Navigation, Pagination]);
 
 
 export const PartnersComponent = ({ partnersContent }) => {
 
   const { clients, partners } = partnersContent;
+  const slidesClient = [];
+  const slidesPartner = [];
+
+  useEffect(() => {
+    getSliders();
+  }, []);
+
+  const getSliders = () => {
+    clients.images.forEach((image, i) => {
+      slidesClient.push(
+        <SwiperSlide key={`slide-${i}`} style={{ textAlign: 'center' }}  >
+          <SliderPartnerComponent sliderPartnerContent={image} />
+        </SwiperSlide>
+      )
+    });
+
+    partners.images.forEach((image, i) => {
+      slidesPartner.push(
+        <SwiperSlide key={`slide-${i}`} style={{ textAlign: 'center' }} >
+          <SliderPartnerComponent sliderPartnerContent={image} />
+        </SwiperSlide>
+      )
+    });
+  }
 
   return (
     <>
@@ -24,7 +54,7 @@ export const PartnersComponent = ({ partnersContent }) => {
         <Grid item xs={5} md={8} />
       </Grid>
       <Grid container direction="row" justify="center" alignItems="center" style={{ marginTop: '50px', marginBottom: '50px' }} >
-        {
+        {/* {
           clients.images.map((image, index) => {
             return (
               <Grid key={`Photio-Client-${index}`} item xs={8} md={5} style={{ textAlign: 'center', maxWidth: '90%' }} >
@@ -32,7 +62,23 @@ export const PartnersComponent = ({ partnersContent }) => {
               </Grid>
             )
           })
-        }
+        } */}
+        <Grid item xs={10} md={8} style={{ maxWidth: '100vw' }}>
+        <Hidden smDown>
+        <Swiper id="main" pagination tag="section" wrapperTag="ul" slidesPerView={2} spaceBetween={30} >
+            {
+              slidesClient
+            }
+          </Swiper>
+        </Hidden>
+        <Hidden mdUp>
+        <Swiper id="main" pagination tag="section" wrapperTag="ul" slidesPerView={1} spaceBetween={10} >
+            {
+              slidesClient
+            }
+          </Swiper>
+        </Hidden>
+        </Grid>
       </Grid>
       <Grid container direction="row" justify="center" alignItems="flex-start" >
         <Grid item xs={0} md={1} style={{ borderBottom: `solid 3px ${colors.green_2}`, marginTop: '87px' }} />
@@ -49,7 +95,7 @@ export const PartnersComponent = ({ partnersContent }) => {
         <Grid item xs={1} md={8} />
       </Grid>
       <Grid container direction="row" justify="center" alignItems="center" style={{ marginTop: '50px', marginBottom: '50px' }} >
-        {
+        {/* {
           partners.images.map((image, index) => {
             return (
               <Grid key={`Photio-Partner-${index}`} item xs={5} md={3} style={{ textAlign: 'center', maxWidth: '90%' }} >
@@ -57,7 +103,23 @@ export const PartnersComponent = ({ partnersContent }) => {
               </Grid>
             )
           })
-        }
+        } */}
+        <Grid item xs={10} md={8} style={{ maxWidth: '100vw' }}>
+          <Hidden smDown>
+          <Swiper id="main" pagination tag="section" wrapperTag="ul" slidesPerView={3} spaceBetween={30} >
+            {
+              slidesPartner
+            }
+          </Swiper>
+          </Hidden>
+          <Hidden mdUp>
+          <Swiper id="main" pagination tag="section" wrapperTag="ul" slidesPerView={1} spaceBetween={10} >
+            {
+              slidesPartner
+            }
+          </Swiper>
+          </Hidden>
+        </Grid>
       </Grid>
     </>
   )
---
+++ src/components/home/SliderNewsComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';	
+
+export const SliderNewsComponent = ({sliderPartnerContent}) => {
+  return (	
+    <img src={sliderPartnerContent} alt="Photio-Client" style={{maxWidth: '80%', textAlign: 'center', height: '100px', marginLeft: '-20%'}} />
+  )	
+}
