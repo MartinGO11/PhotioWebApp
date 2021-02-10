d6ab6dcc1171d495fc60430589efe3d5766d9ba0
sergioeabarcaf <sergioaf1991@gmail.com>
2021-02-10 12:09:34 -0300

    FEAT: add slider in NewsComponent

--- src/components/home/NewsResumenComponents.jsx
+++ src/components/home/NewsResumenComponents.jsx
@@ -1,35 +1,68 @@
-import { Grid } from '@material-ui/core'
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Grid, Hidden } from '@material-ui/core'
 import homeStyle from '../../styles/global/homeStyle';
 import { NewsCardComponent } from './NewsCardComponent';
 import colors from '../../styles/global/colors';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import { SliderNewsComponent } from './SliderNewsComponent';
+
+SwiperCore.use([Navigation, Pagination]);
 
 export const NewsResumenComponents = ({ newsText }) => {
 
   const homeStyles = homeStyle();
+  const slidesNews = [];
+
+  useEffect(() => {
+    getSliders();
+  }, []);
+
+  const getSliders = () => {
+    newsText.news.forEach((article, i) => {
+      slidesNews.push(
+        <SwiperSlide key={`slide-${i}`} style={{ textAlign: 'center' }}  >
+          <SliderNewsComponent sliderNewsContent={article} />
+        </SwiperSlide>
+      )
+    });
+    console.log(slidesNews);
+  }
 
   return (
     <>
       <Grid container direction="row" justify="flex-start" alignItems="center" className={homeStyles.newsResumenComponent} >
         <Grid item xs={1} />
         <Grid item xs={10} md={2} >
-          <p style={{color: colors.blue_1, fontSize: '48px', fontWeight: 'bold', marginBottom: '0px'}}>NOTICIAS</p>
-          <div style={{fontSize: '18px', textTransform: 'uppercase'}}>
+          <p style={{ color: colors.blue_1, fontSize: '48px', fontWeight: 'bold', marginBottom: '0px' }}>NOTICIAS</p>
+          <div style={{ fontSize: '18px', textTransform: 'uppercase' }}>
             {newsText.description}
           </div>
         </Grid>
       </Grid>
 
-      <Grid container direction="row" justify="flex-start" alignItems="center" className={homeStyles.newsResumenComponent} >
-        {
-          newsText.news.map(article => {
-            return (
-              <Grid key={article.id} item xs={12} md={4}>
-                <NewsCardComponent newsContent={article} />
-              </Grid>
-            )
-          })
-        }
+      <Grid container direction="row" justify="center" alignItems="center" className={homeStyles.newsResumenComponent} >
+        <Hidden smDown>
+          {
+            newsText.news.map(article => {
+              return (
+                <Grid key={article.id} item xs={10} md={4}>
+                  <NewsCardComponent newsContent={article} />
+                </Grid>
+              )
+            })
+          }
+        </Hidden>
+        <Hidden mdUp>
+          <Grid item xs={10} md={4}>
+            <Swiper id="main" pagination tag="section" wrapperTag="ul" slidesPerView={1} spaceBetween={10} style={{maxWidth: '100vw'}} >
+              {
+                slidesNews
+              }
+            </Swiper>
+          </Grid>
+        </Hidden>
       </Grid>
     </>
   )
--- src/components/home/SliderNewsComponent.jsx
+++ src/components/home/SliderNewsComponent.jsx
@@ -1,7 +1,40 @@
-import React from 'react';	
+import React from 'react';
+import { Paper, Grid } from '@material-ui/core';
+import colors from '../../styles/global/colors'
 
-export const SliderNewsComponent = ({sliderPartnerContent}) => {
-  return (	
-    <img src={sliderPartnerContent} alt="Photio-Client" style={{maxWidth: '80%', textAlign: 'center', height: '100px', marginLeft: '-20%'}} />
-  )	
+export const SliderNewsComponent = ({ sliderNewsContent }) => {
+
+  const styles = {
+    paperContainer: {
+      backgroundImage: `url(${Image})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      minHeight: '300px',
+      display: 'flex',
+      alignItems: 'center',
+      maxWidth: '100%',
+      marginLeft: '-10%'
+    }
+  };
+
+  const dateArray = sliderNewsContent.date.split('-');
+
+  return (
+    <Paper elevation={0} style={styles.paperContainer}>
+      <Grid container direction="row" justify="flex-start" alignItems="flex-start" >
+        <Grid item xs={1} />
+        <Grid item xs={1} style={{ color: colors.white, borderBottom: '3px solid #FFFFFF' }}>
+          <p style={{ fontSize: '18px', margin: '0px', fontWeight: 'bold' }} > {dateArray[0]} </p>
+          <p style={{ fontSize: '18px', margin: '0px', fontWeight: 'bold', textTransform: 'uppercase' }}> {dateArray[1]} </p>
+        </Grid>
+        <Grid item xs={1} />
+        <Grid item xs={8} style={{ color: colors.white, marginTop: '30px' }}>
+          <p style={{ fontSize: '18px', fontWeight: 'bold', marginBottom: '0px' }}>{sliderNewsContent.title} </p>
+          <p style={{ fontSize: '14px', textAlign: 'right', marginTop: '0px' }}> {sliderNewsContent.origin} </p>
+          <p style={{ fontSize: '14px', background: 'rgba(24,70,102,0.3)', padding: '5px' }} >{sliderNewsContent.description}</p>
+        </Grid>
+      </Grid>
+    </Paper>
+  )
 }
