5ce15e16c649b87dfdf1369f53b92b40ddc64def
sergioeabarcaf <sergioaf1991@gmail.com>
2021-03-05 00:31:14 -0300

    FEAT: use environment for secrets

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -1,3 +1,14 @@
-export const sendFormValues = ( body ) => {
-  console.log(body);
+export const sendFormValues = async ( body ) => {
+  const path = process.env.REACT_APP_FORM_PATH;
+  const apiKey = process.env.REACT_APP_APIKEY;
+
+  const response = await fetch(path, {
+    method: 'post',
+    headers: {
+      'X-API-Key': apiKey,
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(body)
+  });
+  console.log(response.json());
 }
