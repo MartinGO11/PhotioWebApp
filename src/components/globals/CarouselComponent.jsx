767e9cf4936c8f1205be1051d2665c7ba98827bc
sergioeabarcaf <sergioaf1991@gmail.com>
2021-05-25 17:26:50 -0400

    FIX: change colors in sideMenu Responsive

--- src/components/globals/CarouselComponent.jsx
+++ src/components/globals/CarouselComponent.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import { Grid, Hidden } from '@material-ui/core';
-import logoPhotio from './logoPhotioCarousel.svg';
-import MenuIcon from '@material-ui/icons/Menu';
-import { default as navbarItems } from '../../constants/navbarItems.json';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import { Grid, Hidden } from "@material-ui/core";
+import logoPhotio from "./logoPhotioCarousel.svg";
+import MenuIcon from "@material-ui/icons/Menu";
+import { default as navbarItems } from "../../constants/navbarItems.json";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles({
   list: {
     width: 250,
   },
   fullList: {
-    width: 'auto',
+    width: "auto",
   },
 });
 
 export const CarouselComponent = ({ carouselContent }) => {
-
-
   const classes = useStyles();
   const [state, setState] = React.useState({
     top: false,
@@ -33,7 +30,11 @@ export const CarouselComponent = ({ carouselContent }) => {
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
@@ -43,12 +44,22 @@ export const CarouselComponent = ({ carouselContent }) => {
   const list = (anchor) => (
     <div
       className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
       })}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
+      style={{ backgroundColor: "#184666", height: "100%" }}
     >
+      <img
+        src={logoPhotio}
+        alt="Photio-Logo"
+        style={{
+          margin: "10% 35%",
+          maxWidth: "30%",
+        }}
+      />
+
       <List>
         {navbarItems.map((item, index) => (
           <ListItem button key={item.title}>
@@ -58,6 +69,7 @@ export const CarouselComponent = ({ carouselContent }) => {
               className="nav-item nav-link"
               exact
               to={item.to}
+              style={{ textDecoration: "none", color: "white" }}
             >
               {item.title}
             </NavLink>
@@ -68,50 +80,52 @@ export const CarouselComponent = ({ carouselContent }) => {
     </div>
   );
 
-
-
   return (
     <Grid container direction="row" justify="center" alignItems="center">
       <Grid item xs={12}>
-        <div style={{
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundImage: `url(${carouselContent[0].background})`,
-          height: '90vh',
-        }}>
+        <div
+          style={{
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundImage: `url(${carouselContent[0].background})`,
+            height: "90vh",
+          }}
+        >
           <Hidden smDown>
             <NavLink
               activeClassName="active"
               className="nav-item nav-link"
               exact
               to="/"
-              style={{ textDecoration: 'none', color: 'inherit' }}
+              style={{ textDecoration: "none", color: "inherit" }}
             >
               <img
                 src={logoPhotio}
                 alt="Photio-Logo"
                 style={{
-                  position: 'absolute',
-                  top: '5%',
-                  left: '5%',
-                  marginRight: '10vw',
-                  textAlign: 'left',
-                  maxWidth: '8%'
+                  position: "absolute",
+                  top: "5%",
+                  left: "5%",
+                  marginRight: "10vw",
+                  textAlign: "left",
+                  maxWidth: "8%",
                 }}
               />
             </NavLink>
-            <p style={{
-              color: 'white',
-              fontSize: '16px',
-              padding: '40px 2vw 40px 30px',
-              background: 'rgba(24,70,102,0.8)',
-              position: 'absolute',
-              top: '60vh',
-              left: '50%',
-              textAlign: 'left',
-              width: '50%'
-            }}>
+            <p
+              style={{
+                color: "white",
+                fontSize: "16px",
+                padding: "40px 2vw 40px 30px",
+                background: "rgba(24,70,102,0.8)",
+                position: "absolute",
+                top: "60vh",
+                left: "50%",
+                textAlign: "left",
+                width: "50%",
+              }}
+            >
               {carouselContent[0].description}
             </p>
           </Hidden>
@@ -121,57 +135,59 @@ export const CarouselComponent = ({ carouselContent }) => {
               className="nav-item nav-link"
               exact
               to="/"
-              style={{ textDecoration: 'none', color: 'inherit' }}
+              style={{ textDecoration: "none", color: "inherit" }}
             >
               <img
                 src={logoPhotio}
                 alt="Photio-Logo"
                 style={{
-                  position: 'absolute',
-                  top: '5%',
-                  left: '10%',
-                  marginRight: '10vw',
-                  textAlign: 'left',
-                  maxWidth: '20%'
+                  position: "absolute",
+                  top: "5%",
+                  left: "10%",
+                  marginRight: "10vw",
+                  textAlign: "left",
+                  maxWidth: "20%",
                 }}
               />
             </NavLink>
             <>
-              <MenuIcon style={{
-                fontSize: '30px',
-                color: 'white',
-                position: 'absolute',
-                top: '5%',
-                left: '85%'
-              }}
-                onClick={toggleDrawer('right', true)}
+              <MenuIcon
+                style={{
+                  fontSize: "30px",
+                  color: "white",
+                  position: "absolute",
+                  top: "5%",
+                  left: "85%",
+                }}
+                onClick={toggleDrawer("right", true)}
               />
               <SwipeableDrawer
-                anchor={'right'}
-                open={state['right']}
-                onClose={toggleDrawer('right', false)}
-                onOpen={toggleDrawer('right', true)}
+                anchor={"right"}
+                open={state["right"]}
+                onClose={toggleDrawer("right", false)}
+                onOpen={toggleDrawer("right", true)}
               >
-                {list('right')}
+                {list("right")}
               </SwipeableDrawer>
             </>
 
-            <p style={{
-              color: 'white',
-              fontSize: '14px',
-              padding: '20px 30px 20px 30px',
-              background: 'rgba(24,70,102,0.8)',
-              position: 'absolute',
-              top: '50%',
-              left: '20%',
-              textAlign: 'left'
-            }}>
+            <p
+              style={{
+                color: "white",
+                fontSize: "14px",
+                padding: "20px 30px 20px 30px",
+                background: "rgba(24,70,102,0.8)",
+                position: "absolute",
+                top: "50%",
+                left: "20%",
+                textAlign: "left",
+              }}
+            >
               {carouselContent[0].description}
             </p>
           </Hidden>
-
         </div>
-      </Grid >
-    </Grid >
-  )
-}
+      </Grid>
+    </Grid>
+  );
+};
